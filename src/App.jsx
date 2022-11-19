6acc6fcd207b7243148344fb6e6a044f9803a647
Matvey <morozovmatvej75@gmail.com>
2022-11-19 18:13:51 +0300

    roll dice works

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,48 @@
+import { useEffect } from 'react';
 import { useState } from 'react'
+import { nanoid } from "nanoid"
+import Die from './Die';
 
 export default function App() {
+    const [dice, setDice] = useState(initialDice());
+    
+    function initialDice() {
+      let newDice = [];
+      for (let i = 0; i < 10; i++) {
+        newDice.push(
+          {
+            id: nanoid(),
+            number: Math.floor(Math.random() * 6) + 1,
+            isFrozen: false
+          }
+        )
+      }
+      return newDice;
+    }
+
+    function updateDice() {
+      setDice(prevState => {
+        return prevState.map(item => {
+          if(!item.isFrozen) item.number = Math.floor(Math.random() * 6) + 1;
+          return item;
+        })
+      });
+    }
+
+    function toggleFreeze(id) {
+      
+    }
+
+    const diceNodes = dice.map((die, i) => {
+      return <Die
+        key={i}
+        id={die.id}
+        number={die.number}
+        isFrozen={die.isFrozen}
+        toggleFreeze={toggleFreeze}
+      />
+    })
+
     return (
       <div className='app'>
         <div className='game'>
@@ -11,10 +53,10 @@ export default function App() {
               </p>
             </div>
             <div className="dice">
-
+              {diceNodes}
             </div>
             <div className="roll-button">
-              <button>Roll</button>
+              <button onClick={updateDice}>Roll</button>
             </div>
         </div>
       </div>

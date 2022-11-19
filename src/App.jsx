b49528520e1654f04a477f88cc9b5070fd0a8a3e
Matvey <morozovmatvej75@gmail.com>
2022-11-19 14:46:13 +0300

    More CSS & html

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    return (
+      <div className='app'>
+        <div className='game'>
+            <div className='title-rules'>
+              <h1>Tenzies</h1> 
+              <p>
+                Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+              </p>
+            </div>
+            <div className="dice">
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+            </div>
+            <div className="roll-button">
+              <button>Roll</button>
+            </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    )
 }
-
-export default App

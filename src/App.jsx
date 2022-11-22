d4aa22f61347dd090c593fe63d5124b22bf7f6fc
Matvey <67281212+matvey733@users.noreply.github.com>
2022-11-22 11:55:53 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -98,7 +98,7 @@ export default function App() {
       <Confetti 
         width={windowDimensions.width}
         height={windowDimensions.height}
-        numberOfPieces={won ? 600 : 0}
+        numberOfPieces={won ? 100000 : 0}
         initialVelocity={50}
       />
       <div className="game">

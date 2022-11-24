965ee426ddf03e2d3363c99688abe8bfe4dd39e1
Matvey <morozovmatvej75@gmail.com>
2022-11-24 17:17:30 +0300

    show best time on title screen

--- src/TitleScreen.jsx
+++ src/TitleScreen.jsx
@@ -4,15 +4,19 @@ import { useNavigate } from "react-router-dom";
 export default function TitleScreen() {
     const navigate = useNavigate();
 
+    const prevBestTime = localStorage.getItem("bestTime");
+
     return (
             <div className="container title-screen">
                 <h1 className="game-title">Tenzies</h1>
+                {prevBestTime && <h2 className="prev-best-time">Your best time: {prevBestTime}</h2>}
                     <p className="rules">
                     Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+                        <p className="hint">
+                            Hint: use spacebar to roll the dice.
+                        </p>
                     </p>
-                    <p className="hint">
-                        Hint: use spacebar to roll the dice.
-                    </p>
+                    
 
                 <div className="buttons-container">
                     <button

34c5e1e96f7805f781e7eaf22d81c8eef0fd1a85
Matvey <67281212+matvey733@users.noreply.github.com>
2022-12-06 12:33:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import TitleScreen from "./TitleScreen"
 import Settings from "./Settings"
 import Game from "./game/Game"
+import NotFound from ./NotFound
 import {
   Routes,
   Route
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<TitleScreen />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/game" element={<Game />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
   )
